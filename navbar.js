84366c673d0142612637795caa6e684c591ce43d
jatinparmar24 <jatinparmar2424@gmail.com>
2025-08-06 21:44:16 +0530

    new navbar design

--- navbar.js
+++ navbar.js
@@ -2,3 +2,14 @@ function toggleMenu(position) {
   const menu = document.querySelector(`.multi-links-${position}`);
   menu.classList.toggle('show');
 }
+// first section
+
+// second section
+function toggleMenus(scope) {
+  const links = document.querySelector(`.multi-nav-links.${scope}`);
+  const auth = document.querySelector(`.multi-nav-auth.${scope}`);
+  links.classList.toggle("show");
+  auth.classList.toggle("show");
+}
+
+// second section
