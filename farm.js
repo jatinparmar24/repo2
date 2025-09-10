947f764e5556d54793d842633ceb597a1bfd3b7f
jatinparmar24 <jatinparmar2424@gmail.com>
2025-09-10 13:47:23 +0530

    second section added

--- farm.js
+++ farm.js
@@ -5,3 +5,17 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   mobileMenu.classList.toggle("active");
 });
+
+// second section
+document.addEventListener("scroll", () => {
+  const reveals = document.querySelectorAll(".spa-reveal");
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("spa-active");
+    }
+  });
+});
+
